601c173f1d41746c21835853fd01e80fcdffaf56
Mohammed Zayan <zayanit@gmail.com>
2020-10-25 14:35:00 +0300

    data & Methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--{{ title }}-->
+    <!--<input type="text" v-bind:value="formData.name">-->
+    <input type="text" v-model="title">
+    <HelloWorld :msg="title"/>
+    <button v-on:click="changeTitle('This is a new changed title')">Change Title</button>
   </div>
 </template>
 
@@ -13,6 +17,17 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      title: 'This is a good title for Vue.js',
+      formData: { name: 'Mohammed', address: '' }
+    }
+  },
+  methods: {
+    changeTitle (newTitle) {
+      this.title = newTitle
+    }
   }
 }
 </script>
