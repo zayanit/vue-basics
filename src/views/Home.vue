91b82fd7fa6a6c04b0b3b13e5469bedabe39d0eb
Mohammed Zayan <zayanit@gmail.com>
2020-10-25 20:29:24 +0300

    Watcher & Computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,14 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <!--{{ title }}-->
     <!--<input type="text" v-bind:value="formData.name">-->
-    <input type="text" v-model="title">
+    <!--{{ brandNewTitle }}
+    <input type="text" v-model="title">-->
+    <br>
+    <label>Frist Name: </label><input type="text" v-model="firstName">
+    <br>
+    <label>Last Name: </label><input type="text" v-model="lastName">
+    <br>
+    <p>The fullName is: {{ fullName }}</p>
     <HelloWorld :msg="title"/>
     <button v-on:click="changeTitle('This is a new changed title')">Change Title</button>
   </div>
@@ -21,12 +28,32 @@ export default {
   data () {
     return {
       title: 'This is a good title for Vue.js',
-      formData: { name: 'Mohammed', address: '' }
+      brandNewTitle: '',
+      formData: { name: 'Mohammed', address: '' },
+      firstName: '',
+      lastName: ''
     }
   },
   methods: {
     changeTitle (newTitle) {
       this.title = newTitle
+    }
+  },
+  watch: {
+    title: function (val, oldVal) {
+      // console.log('The value changed from: ' + oldVal + ' to: ' + val)
+      this.brandNewTitle = '{' + val + '}'
+    }/* ,
+    firstName: function (val) {
+      this.fullName = val + ' ' + this.lastName
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + ' ' + val
+    } */
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
     }
   }
 }
